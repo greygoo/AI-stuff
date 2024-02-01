43dd8df3a068fe5f6cda441e6d2a8f60e877f42a
Christian Hueller <chuller@suse.com>
2024-02-01 12:30:56 +0100

    Add files

---
+++ ask.py
@@ -0,0 +1,47 @@
+"""
+This script reads the database of information from local text files
+and uses a large language model to answer questions about their content.
+"""
+
+from langchain_community.llms import CTransformers
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+
+# prepare the template we will use when prompting the AI
+template = """Use the following pieces of information to answer the user's question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+Context: {context}
+Question: {question}
+Only return the helpful answer below and nothing else.
+Helpful answer:
+"""
+
+# load the language model
+llm = CTransformers(model='./llama-2-7b-chat.ggmlv3.q8_0.bin',
+                    model_type='llama',
+                    config={'max_new_tokens': 256, 'temperature': 0.01})
+
+# load the interpreted information from the local database
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2",
+    model_kwargs={'device': 'cpu'})
+db = FAISS.load_local("faiss", embeddings)
+
+# prepare a version of the llm pre-loaded with the local content
+retriever = db.as_retriever(search_kwargs={'k': 2})
+prompt = PromptTemplate(
+    template=template,
+    input_variables=['context', 'question'])
+qa_llm = RetrievalQA.from_chain_type(llm=llm,
+                                     chain_type='stuff',
+                                     retriever=retriever,
+                                     return_source_documents=True,
+                                     chain_type_kwargs={'prompt': prompt})
+
+# ask the AI chat about information in our local files
+print("Ask a question");
+prompt = str(input())
+output = qa_llm({'query': prompt})
+print(output["result"])
---
+++ read.py
@@ -0,0 +1,25 @@
+"""
+This script creates a database of information gathered from local text files.
+"""
+
+from langchain_community.document_loaders import DirectoryLoader, TextLoader, PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+# define what documents to load
+#loader = DirectoryLoader("./input/txt", glob="*.txt", loader_cls=TextLoader)
+loader = DirectoryLoader("./input/pdf", glob="*.pdf", loader_cls=PyPDFLoader)
+
+# interpret information in the documents
+documents = loader.load()
+splitter = RecursiveCharacterTextSplitter(chunk_size=500,
+                                          chunk_overlap=50)
+texts = splitter.split_documents(documents)
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2",
+    model_kwargs={'device': 'cpu'})
+
+# create and save the local database
+db = FAISS.from_documents(texts, embeddings)
+db.save_local("faiss")
